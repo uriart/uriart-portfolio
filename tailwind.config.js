37484f9289b4f51535b35d3ff045365a48c1d700
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-06-23 13:26:00 +0200

    main: inicio diseño portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'image-home': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/assets/bg-image.png')",
+        'photo': "url('/assets/profile-photo.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
