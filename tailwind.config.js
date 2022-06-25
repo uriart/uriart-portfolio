9380f3ef00481d8b8795e3858adaedbb147e8960
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-06-25 18:20:32 +0200

    main: mejoras responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       backgroundImage: {
         'image-home': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/assets/bg-image.png')",
         'photo': "url('/assets/profile-photo.jpg')",
+        'darkness': "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))",
       }
     },
   },
