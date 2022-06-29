9eb4ddc7ae92ca92b5d4e0eb9829e18627b41373
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-06-29 23:06:16 +0200

    main: contact section + improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'image-home': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/assets/bg-image.png')",
+        'image-home': "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/assets/bg-image.png')",
         'photo': "url('/assets/profile-photo.jpg')",
         'darkness': "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))",
-      }
+      },
+      backdropBlur: {
+        xs: '1px',
+      },
     },
   },
   plugins: [],
