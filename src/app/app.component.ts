330a9f4fd9899507af3acba8eb181ffe31dc256b
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-07-04 11:58:41 +0200

    main: primera release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'uriart-portfolio';
+  observer: any;
+  
+  ngOnInit() {
+    this.intersectionObserver();
+  }
+
+  ngAfterViewInit() {
+    let sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+      this.observer.observe(section);
+    });
+  }
+
+  intersectionObserver() {
+    let options = {
+      rootMargin: '0px',
+      threshold: 0.6,
+    };
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.intersectionHandler(entry);
+        }
+      });
+    }, options);
+  }
+
+  intersectionHandler(entry:any) {
+    const id = entry.target.id;
+    this.removeActiveClass();
+    this.setActiveClass(id);
+    this.menuOpacityHandler(id);
+  }
+
+  removeActiveClass() {
+    const menuList = document.querySelectorAll("li");
+    menuList.forEach((menuItem) => {
+      menuItem?.classList.remove("text-green-600");
+    });
+  }
+
+  setActiveClass(id:string) {
+    const intersectioned = document.querySelector("li[data-ref=" + id + "]");
+    null != intersectioned && intersectioned.classList.add("text-green-600");
+  }
+
+  menuOpacityHandler(id:string) {
+    const navbar = document.querySelector("nav");
+    if(id == 'home')
+      navbar?.classList.remove('bg-image-header');
+    else
+      navbar?.classList.add('bg-image-header');
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,22 @@ module.exports = {
     extend: {
       backgroundImage: {
         'image-home': "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/assets/bg-image.png')",
-        'image-header': "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))",
+        'image-header': "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))",
         'photo': "url('/assets/profile-photo.jpg')",
         'darkness': "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))",
       },
       backdropBlur: {
         xs: '1px',
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 0.5s infinite',
+      }
     },
   },
   plugins: [],
